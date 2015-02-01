f3a76561cd6d3bc68ca8e77c6007e738608c3670
Thomas Mullaly <tmullaly88@gmail.com>
2015-01-31 19:23:04 -0600

    Added project

---
+++ LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingInterview
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
